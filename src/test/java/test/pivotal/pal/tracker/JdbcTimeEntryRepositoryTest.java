cf0ceb183e4693cd1e0a5a4206d98c762b539bde
saravana.g@cognizant.com <Libnov@19>
2020-02-29 13:46:36 -0500

    Health Monitoring

--- src/test/java/test/pivotal/pal/tracker/JdbcTimeEntryRepositoryTest.java
+++ src/test/java/test/pivotal/pal/tracker/JdbcTimeEntryRepositoryTest.java
@@ -39,7 +39,7 @@ public class JdbcTimeEntryRepositoryTest {
         TimeEntry newTimeEntry = new TimeEntry(123, 321, LocalDate.parse("2017-01-09"), 8);
         TimeEntry entry = subject.create(newTimeEntry);
 
-        Map<String, Object> foundEntry = jdbcTemplate.queryForMap("SELECT * FROM time_entries WHERE id = ?", entry.getId());
+        Map<String, Object> foundEntry = jdbcTemplate.queryForMap("Select * from time_entries where id = ?", entry.getId());
 
         assertThat(foundEntry.get("id")).isEqualTo(entry.getId());
         assertThat(foundEntry.get("project_id")).isEqualTo(123L);
